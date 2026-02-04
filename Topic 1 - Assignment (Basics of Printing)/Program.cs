4ad7675e420d895cc799ddf6759b3367305454d7
700014473 <700014473@2260I-L29-008>
2026-02-04 13:05:20 -0500

    sup?

--- Topic 1 - Assignment (Basics of Printing)/Program.cs
+++ Topic 1 - Assignment (Basics of Printing)/Program.cs
@@ -4,23 +4,74 @@
     {
         static void Main(string[] args)
         {
-            // Angelpreet AKA Angel
+            // Angelpreet AKA Angel   
+
+            Console.Title = "Topic 1 - Assignment (Basics of Printing)";    
+
+            Console.ForegroundColor = ConsoleColor.Cyan;    
+            Console.BackgroundColor = ConsoleColor.Black;
 
             Console.WriteLine("Good day! My name is Angel!");
             Console.WriteLine(" ");
             Console.WriteLine("There are two reasons why programmers utilize comments. First reason would be to hide certain code from appearing in the output. The second reason would be to create a note for oneself.");
             Console.WriteLine();
-            Console.WriteLine("  A     TTTTT");
-            Console.WriteLine(" A A      T");
 
-            // Is the sky really blue? No, there are blue, violet, and red rays in the sky. However, our eyes see blue better than red which is why the sky appears blue. Something like this...don't trust me on it though.
+            Console.WriteLine("Press ENTER to see my initials in ASCII art!");
+            Console.ReadLine(); 
 
-            Console.WriteLine("A   A     T");
-            Console.WriteLine("AAAAA     T");
-            Console.WriteLine("A   A     T");
-            Console.WriteLine("A   A     T");
-            Console.WriteLine("A   A     T");
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.ForegroundColor = ConsoleColor.White;
 
+            Console.Write("  A     ");
+
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+
+            Console.WriteLine("TTTTT");
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.Write(" A A    ");
+
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+
+            Console.WriteLine("  T  ");
+
+            /* Is the sky really blue? No, there are blue, violet, and red rays in the sky. 
+             * However, our eyes see blue better than red which is why the sky appears blue. 
+             * Something like this...don't trust me on it though .
+            */
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.Write("AAAAA   ");
+
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+
+            Console.WriteLine("  T  ");
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.Write("A   A   ");  
+
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+
+            Console.WriteLine("  T  ");
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.Write("A   A   ");
+
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+
+            Console.WriteLine("  T  ");
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.Write("A   A   ");
+
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+
+            Console.WriteLine("  T  ");
 
         }
     }
