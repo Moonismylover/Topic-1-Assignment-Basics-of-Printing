8a184bfb37dcd280b6a0085474d43d7d6fe30686
700014473 <700014473@2260I-L29-008>
2026-02-04 12:38:40 -0500

    Add project files.

---
+++ Topic 1 - Assignment (Basics of Printing)/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Topic_1___Assignment__Basics_of_Printing_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Angelpreet AKA Angel
+
+            Console.WriteLine("Good day! My name is Angel!");
+            Console.WriteLine(" ");
+            Console.WriteLine("There are two reasons why programmers utilize comments. First reason would be to hide certain code from appearing in the output. The second reason would be to create a note for oneself.");
+            Console.WriteLine();
+            Console.WriteLine("  A     TTTTT");
+            Console.WriteLine(" A A      T");
+
+            // Is the sky really blue? No, there are blue, violet, and red rays in the sky. However, our eyes see blue better than red which is why the sky appears blue. Something like this...don't trust me on it though.
+
+            Console.WriteLine("A   A     T");
+            Console.WriteLine("AAAAA     T");
+            Console.WriteLine("A   A     T");
+            Console.WriteLine("A   A     T");
+            Console.WriteLine("A   A     T");
+
+
+        }
+    }
+}
